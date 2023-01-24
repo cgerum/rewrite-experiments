72e0be66431bc70480b9aace5fc1ddb38b930132
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2023-01-24 03:09:35 +0100

    [CI] install updated dependencies

--- rewrite-eqsat/src/lib.rs
+++ rewrite-eqsat/src/lib.rs
@@ -1,3 +1,5 @@
 pub mod language;
 pub mod rules;
 pub mod analysis;
+#[cfg(feature = "tvm")]
+pub mod tvm;
--- rewrite-eqsat/src/main.rs
+++ rewrite-eqsat/src/main.rs
@@ -1,3 +1,5 @@
+use std::path::PathBuf;
+
 use docopt::Docopt;
 use serde::Deserialize;
 use egg::*;
@@ -6,25 +8,31 @@ use rewrite_eqsat::analysis::TensorAnalysis;
 use rewrite_eqsat::rules::*;
 use rewrite_eqsat::language::TensorLang;
 
+#[cfg(feature = "tvm")]
+use rewrite_eqsat::tvm::from_relay;
+
 const USAGE: &'static str = "
 Equality Saturation based prototype for graph rewriting
 
 Usage:
     eqsat (-h | --help)
     eqsat [-s | --stats] [--explain] <expression>
+    eqsat [-s | --stats] [--explain] -f <file>
 
 Options:
   -h --help     Show this screen.
   --explain     Show list of transformations for this exception.
   -s --stats    Print statistics considering the size of the used egraph, 
                 and the runtime of equality saturation
+  -f --file     relay file to run optimization over
 ";
 
 #[derive(Debug, Deserialize)]
 struct Args {
     flag_explain: bool,
     flag_stats: bool, 
-    arg_expression: String, 
+    arg_expression: String,
+    arg_file: PathBuf,
 }
 
 fn main() {
@@ -32,8 +40,10 @@ fn main() {
         .and_then(|d| d.deserialize())
         .unwrap_or_else(|e| e.exit());
     
-
-    let start: RecExpr<TensorLang> = args.arg_expression.parse().unwrap();
+    let mut start = "a".parse().unwrap();
+    if args.arg_expression != "" {
+        start = args.arg_expression.parse().unwrap();
+    }
 
     let rules = all_rules();
 
